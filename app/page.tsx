65e6d40bc3a383b618fbc5558aafda735510e305
MkrisDK <mkrisprivat@gmail.com>
2025-02-01 12:11:21 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import CurrencyConverter from '@/components/CurrencyConverter';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4 md:p-8">
+      <CurrencyConverter />
+    </main>
+  );
+}
