f1e770ee3761d6d7d02549c3fc9823910b0eda04
MkrisDK <mkrisprivat@gmail.com>
2025-02-01 12:12:48 +0100

    Create layout.tsx

---
+++ app/dkk-til-usd/layout.tsx
@@ -0,0 +1,5 @@
+export const metadata = {
+  title: 'DKK til USD Valutaomregner | Live Valutakurser',
+  description: 'Omregn danske kroner (DKK) til amerikanske dollars (USD) med vores live valutaomregner. Få de seneste valutakurser og præcise udregninger.',
+  keywords: 'valutaomregner, dkk til usd, danske kroner til dollars, valutakurs',
+};
